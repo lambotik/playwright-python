2146eaa486f26d452147951a604528d6b77262d0
lambotik <lambotik1988@gmail.com>
2024-03-19 16:26:57 +0300

    Playwright

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,13 +8,13 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture()
 def browser():
     with sync_playwright() as browser:
-        if 'CI' in os.environ:
-            browser = browser.chromium.launch(headless=True, slow_mo=10)
-            browser = browser.new_page()
-        else:
-            browser = browser.chromium.launch(headless=False, slow_mo=10)
-            browser = browser.new_page()
-            browser.set_viewport_size(({r"width": 1900, "height": 1020}))
+        # if 'CI' in os.environ:
+        browser = browser.chromium.launch(headless=True, slow_mo=10)
+        browser = browser.new_page()
+        # else:
+        #     browser = browser.chromium.launch(headless=False, slow_mo=10)
+        #     browser = browser.new_page()
+        #     browser.set_viewport_size(({r"width": 1900, "height": 1020}))
         yield browser
         allure.attach(browser.screenshot(type='png'))
         browser.close()
